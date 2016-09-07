b003cf06e12b4643d8fac8d3dfadb54ceb3f3239
gkrishnan724 <gkrishnan724@gmail.com>
2016-09-07 17:01:40 +0530

    Shopping page in angular

---
+++ JS/angular.module.js
@@ -0,0 +1,80 @@
+ var module = angular.module("sampleApp", ['ngRoute']);
+
+    module.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/login', {
+                    templateUrl: 'Templates/login.template.html',
+                    controller: 'loginController'
+                }).
+                when('/Electronics', {
+                    templateUrl: 'Templates/Electronics.template.html',
+                    controller: 'RouteController'
+                }).
+                when('/Books', {
+                    templateUrl: 'Templates/Books.template.html',
+                    controller: 'RouteController'
+                }).
+                when('/Clothes', {
+                    templateUrl: 'Templates/Clothes.template.html',
+                    controller: 'RouteController'
+                }).
+                otherwise({
+                    redirectTo: '/login'
+                });
+        }]);
+
+
+module.controller( 'loginController', function($scope,$http,$location) {
+  
+
+
+	
+	
+   $scope.validate = function(form)
+{
+  var obj = JSON.parse(users);
+ 
+cond = 0;
+
+for(i=0;i<obj.length;i++){
+  
+
+
+
+
+if(form.userid == obj[i].username)
+{
+cond = 1;
+if(form.pswrd == obj[i].password)
+{
+
+$location.path("/Electronics");
+break;
+
+}
+
+else
+{
+alert("Incorrect Password");
+break;
+
+}
+
+
+}
+
+
+
+
+}
+
+if(cond == 0){
+alert("Incorrect Credentials");
+}      
+
+}
+   
+       
+
+});
