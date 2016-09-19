9e253cd576cb48033e0e379bf3b2637077b4956e
gkrishnan724 <gkrishnan724@gmail.com>
2016-09-19 18:39:59 +0530

    some new stuff

--- JS/angular.module.js
+++ JS/angular.module.js
@@ -19,17 +19,27 @@
                     templateUrl: 'Templates/Clothes.template.html',
                     controller: 'JSONController'
                 }).
+                when('/Error', {
+                    templateUrl: 'Templates/Error.template.html',
+                    controller: 'JSONController'
+                }).
+                when('/Bill', {
+                    templateUrl: 'Templates/Bill.template.html',
+                    controller: 'JSONController'
+                }).
                 otherwise({
                     redirectTo: '/login'
                 });
         }]);
 
 
-module.controller( 'loginController', function($scope,$rootScope) {
+module.controller( 'loginController', function($scope,$rootScope,$location) {
   
   
     $rootScope.logpage = 1;
-  
+  $rootScope.success = 0;
+$rootScope.Errorpage = 1;
+
 
 
 	
@@ -51,8 +61,9 @@ if(form.userid == obj[i].username)
 cond = 1;
 if(form.pswrd == obj[i].password)
 {
-
-$location.path("/Electronics");
+$rootScope.username = obj[i].username;
+$rootScope.success = 1;
+$location.path("/Books");
 break;
 
 }
@@ -82,16 +93,164 @@ alert("Incorrect Credentials");
 
 });
 
-module.controller('JSONController',function($scope,$rootScope){
+module.controller('JSONController',function($scope,$rootScope,$location){
+ $rootScope.logpage = 0;
+$rootScope.Errorpage = 0;
+
+if($rootScope.success == 0){
+
+
+$location.path("/Error");
+
+$rootScope.Errorpage = 1;
+$rootScope.logpage = 1;
+};
+
+var currentpage = $location.path();
+
+if(currentpage == "/Bill" && $rootScope.success == 1){
+
+$rootScope.Errorpage = 1;
+$rootScope.logpage = 1;
+
+
+};
   
-  $rootScope.logpage = 0;
+
+
+ 
+
   
   $scope.Electronics = JSON.parse(Electronics);
    $scope.Books = JSON.parse(Books);
    $scope.Clothes = JSON.parse(Clothes);
-  
-  
-  
+
   
   
 });
+
+module.controller('MainController', function($scope,$location){
+
+
+$scope.total = 0;
+$scope.added=[];
+$scope.array = [];
+
+
+
+
+
+currentpage = $location.path();
+console.log(currentpage);
+
+
+
+$scope.AddItem = function(name,price){
+var value = parseFloat(price);
+
+
+ var cart = {
+
+name: name,
+price: value 
+
+
+};
+
+
+$scope.total = $scope.total + value;
+$scope.added.push(cart);
+$scope.array  = $scope.added;
+$scope.len = $scope.added.length
+$scope.Tax = $scope.total*0.1;
+$scope.amt = $scope.Tax + $scope.total;
+$scope.Billtotal = $scope.amt;
+
+console.log($scope.amt);
+
+
+
+};
+
+
+
+$scope.clear = function(){
+
+
+
+$scope.added = [];
+$scope.total = 0;
+$scope.len = $scope.added.length
+$scope.Tax = 0;
+$scope.amt = 0;
+
+
+};
+
+$scope.Bill = function(){
+
+
+
+$location.path("/Bill");
+$('#checkoutModal').modal('hide');
+$scope.added = [];
+$scope.total = 0;
+$scope.len = $scope.added.length
+
+
+
+
+};
+$scope.logout = function(){
+
+
+
+$scope.added = [];
+$scope.total = 0;
+$scope.len = $scope.added.length
+$scope.Tax = 0;
+$scope.amt = 0;
+$rootScope.success = 0;
+
+
+};
+
+
+$scope.DelItem = function(item){
+
+
+
+
+for(i=0;i<$scope.added.length;i++){
+
+if(item.name == $scope.added[i].name){
+
+  var value = ParseFloat(item.price)
+
+
+    $scope.added.splice(i,1);
+    
+    console.log($scope.added);
+    
+    $scope.total = $scope.total - value;
+    $scope.Tax = $scope.total*0.1;
+    $scope.amt = $scope.Tax + $scope.total;
+}
+
+
+
+}
+
+
+$scope.array = $scope.added;
+$scope.Billtotal  = $scope.amt;
+
+
+};
+
+
+
+});
+
+
+
